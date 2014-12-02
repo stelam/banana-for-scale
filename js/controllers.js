1b5f59bfa5438a1da9a33969d587f75857453aa3
Stéphane Lam <stelam@gmail.com>
2014-12-02 12:03:30 -0500

    first release

--- js/controllers.js
+++ js/controllers.js
@@ -1,56 +1,279 @@
-angular.module('converter.controllers', [])
+angular.module('converter.controllers', ['ionic', 'ngCordova', 'angular.filter'])
 
-.controller('ConvertCtrl', function($scope, Units) {
+.controller('ConvertCtrl', function($scope, $rootScope, Units, ConversionLocalStorageService, $timeout, $ionicActionSheet, $cordovaSplashscreen, ConnectionManager, $ionicScrollDelegate, $cordovaDialogs, $cordovaToast, ConversionModel, $ionicSideMenuDelegate, $ionicModal, $ionicPlatform) {
   $scope.units = Units.all();
-  $scope.result = {value: ""};
-  $scope.base = {value : "", lastValue : 0};
-  $scope.lastBase = {value: ""};
-  Units.getByName("inches").then(function(unit){
-  	$scope.base.unit = unit;
-  });
+  $scope.result = {value: "", value2 : ""};
+  $scope.base = {value : "", value2 : "", lastValue : 0};
+  $scope.kind = "length"; // hardcoded for now
+  $scope.conversionHistory = [];
+  $scope.conversion = {}
+  $scope.historyVisible = false;
+  $scope.search = {value : ""};
+  $scope.ConversionModel = ConversionModel;
+  ConversionLocalStorageService.loadHistory($scope);
 
-  Units.getByName("bananas").then(function(unit){
-  	$scope.result.unit = unit;
-  });
+  ConversionLocalStorageService.loadLastUsedUnits($scope).then(function(){}, function(error){
+    Units.getByName("inches").then(function(unit){
+      $scope.base.unit = unit;
+    });
 
+    Units.getByName("bananas").then(function(unit){
+      $scope.result.unit = unit;
+    });
+  })
+
+
+  $timeout(function(){
+    //$ionicSideMenuDelegate.toggleLeft();
+  }, 500)
+  
 
   $scope.convert = function(){
+
   	if ($scope.base.value != ""){
-	  	var baseQty = new Qty($scope.base.value + " " + $scope.base.unit.name);
-	  	$scope.result.value = baseQty.to($scope.result.unit.name).toPrec(0.01).scalar;
-  	}	
+	  	
+	  	try {
+	  		var baseQty = ConversionModel.getBaseQty($scope);
+	  		$scope.result.value = baseQty.to($scope.result.unit.name).scalar;
+
+		  	ConversionModel.setResult($scope);
+
+		  	ConversionLocalStorageService.addToConversionHistory($scope).then(function(conversionHistory){
+		  		$ionicScrollDelegate.resize();
+		  		$scope.conversion = conversionHistory[0];
+		  	});
+
+	  	} catch (exception){
+	  		try {
+	  			$cordovaToast.showShortTop('Invalid value');
+	  		} catch (exception2){
+	  			console.log(exception2);
+	  		}
+	  	}
+
+  	}
+
   }
 
-  $scope.onBaseValueChange = function(){
-	$scope.result.value = ""
 
-	if (typeof $scope.base.value == "undefined" || $scope.base.value > 999999999){
-		$scope.base.value = parseFloat($scope.base.lastValue);
-	} else {
-		$scope.base.lastValue = String($scope.base.value);
-	  	$scope.base.lastValue = $scope.base.lastValue.replace(",", ".").replace(/[^0-9.]/g, '');
-	  	if ($scope.base.lastValue.indexOf(".") > -1 && $scope.base.lastValue.lastIndexOf(".") > $scope.base.lastValue.indexOf("."))
-	  		$scope.base.lastValue = $scope.base.lastValue.slice(0, -1);
-	  	$scope.base.value = parseFloat($scope.base.lastValue);
-	}
+  $scope.init = function(){
+    $ionicModal.fromTemplateUrl('modal-units.html', {
+      scope: $scope,
+      animation: 'slide-in-up'
+    }).then(function(modal) {
+      $scope.modal = modal;
+    });
 
+
+    $ionicPlatform.registerBackButtonAction(function(e){
+      if ($rootScope.backButtonPressedOnceToExit) {
+        ionic.Platform.exitApp();
+      }
+      else if ($rootScope.$viewHistory.backView) {
+        $rootScope.$viewHistory.backView.go();
+      }
+      else {
+        $rootScope.backButtonPressedOnceToExit = true;
+        window.plugins.toast.showShortCenter(
+          "Press back button again to exit",function(a){},function(b){}
+        );
+        setTimeout(function(){
+          $rootScope.backButtonPressedOnceToExit = false;
+        },2000);
+      }
+      e.preventDefault();
+      return false;
+    },101);
+
+  }
+
+  $scope.init();
+
+  $scope.showModal = function(side){
+    $scope.currentUnit = (side == 'base') ? $scope.base.unit : $scope.result.unit;
+    $scope.currentUnit.side = side;
+    $scope.search.value = "";
+    console.log($scope.currentUnit);
+    $scope.modal.show();
+
+  }
+
+  $scope.scrollTop = function() {
+    $ionicScrollDelegate.scrollTop(true);
+  };
+
+  $scope.clearSearch = function() {
+    $scope.search.value = '';
+  };
+
+  $scope.setUnit = function(name){
+    $scope.modal.hide().then(function(){
+      //$scope.modal.remove();
+    });
+
+    Units.getByName(name).then(function(unit){
+      if ($scope.currentUnit.side == 'base'){
+        $scope.base.unit = unit;
+        $scope.onBaseUnitChange();
+        ConversionLocalStorageService.saveLastUsedBaseUnit($scope);
+      }else{
+        $scope.result.unit = unit;
+        ConversionLocalStorageService.saveLastUsedResultUnit($scope);
+
+        $timeout(function(){
+          $scope.convert();
+        }, 400)
+        
+      }
+    });
+  }
+
+
+  $scope.initAdMob = function(){
+  	var ad_units = {
+  		android : {
+  			banner: 'ca-app-pub-0046815007221313/3464924159',
+  		}
+  	};
+  	var admobid = ad_units.android;
+  	AdMob.createBanner( admobid.banner ) 
+  }
+
+  $scope.removeAdMob = function(){
+    try{
+      AdMob.removeBanner();
+    } catch (exception){
+      console.log(exception);
+    }
+  }
+
+
+  $ionicPlatform.ready().then(function(){
+    try {
+      //$cordovaSplashscreen.hide();
+    } catch (exception){
+      console.log(exception);
+    }
+  	//$scope.initApp();
+  })
+
+  $scope.onBaseValueChange = function(tt){
+  	if ($scope.base.value != $scope.base.lastValue)
+		$scope.result.value = "";
+
+	$scope.base.lastValue = String($scope.base.value);
+  	$scope.base.lastValue = $scope.base.lastValue.replace(",", ".").replace(" ", "").replace("undefined", "");
+  	$scope.base.value = ($scope.base.lastValue);
+
+	$scope.base.lastValue2 = String($scope.base.value2);
+  	$scope.base.lastValue2 = $scope.base.lastValue2.replace(",", ".").replace(" ", "").replace("undefined", "");
+  	$scope.base.value2 = ($scope.base.lastValue2);
 
   }
 
   $scope.onTargetUnitChange = function(targetUnitName){
-  	$scope.convert();
+  	if ($scope.base.unit.name == "feet and inches"){ 
+
+  	} else {
+  		$scope.convert();
+  	}
+
+
   }
-  $scope.onBaseUnitChange = function(baseUnitName){
-  	$scope.convert();
+  $scope.onBaseUnitChange = function(){
+  	if ($scope.base.unit.name == "feet and inches"){
+  		$scope.wipeBaseInput('base-input-2');
+  		$scope.wipeBaseInput('base-input');
+  	} else {
+  		//$scope.convert();
+  	}
+
+  	$scope.result.value = "";
   }
 
-  $scope.copyToClipboard = function(value){
+  $scope.resultContextualMenu = function(conversion){
+  	if (conversion.resultUnit == "feet and inches") {
+		buttons = [
+			{ text: 'Copy result value and units to clipboard', copyValue : ConversionModel.rightSideToString(conversion), action : $scope.copyToClipboard },
+			{ text: 'Copy conversion to clipboard', copyValue : ConversionModel.toString(conversion), action : $scope.copyToClipboard }
+		]
+  	} else{
+		buttons = [
+			{ text: 'Copy result value to clipboard', copyValue : conversion.resultValue, action : $scope.copyToClipboard },
+			{ text: 'Copy result value and units to clipboard', copyValue : ConversionModel.resultValueAndUnitString(conversion), action : $scope.copyToClipboard },
+			{ text: 'Copy conversion to clipboard', copyValue : ConversionModel.toString(conversion), action : $scope.copyToClipboard }
+		]
+  	}
+
   	try{
-  		cordova.plugins.clipboard.copy(value);
+		var hideSheet = $ionicActionSheet.show({
+			buttons : buttons,
+			titleText: ConversionModel.toString(conversion),
+			cancelText: 'Cancel',
+
+			cancel: function() {
+			  // add cancel code..
+			},
+			buttonClicked: function(index, button) {
+				button.action(button.copyValue);
+				return true;
+			}
+		});
   	}
   	catch (exception){
   		console.log(exception);
   	}
   }
-  
+
+
+  $scope.copyToClipboard = function(value){
+  	try{
+  		cordova.plugins.clipboard.copy(value);
+  		$cordovaToast.showShortTop("'" + value + "' copied to clipboard");
+  	} 
+  	catch (exception){
+  		console.log("Failed to copy '" + value + "'");
+  		console.log(exception);
+  	}
+  }
+
+  $scope.toggleHistory = function(){
+  	$scope.historyVisible = !$scope.historyVisible;
+  	$ionicScrollDelegate.resize();
+  }
+
+  $scope.deleteHistory = function(){
+  	ConversionLocalStorageService.wipe($scope);
+  	$scope.toggleHistory();
+  }
+
+  $scope.onKeyDown = function(event){
+  	console.log(event.keyCode);
+  	if (event.keyCode === 13 || event.keyCode === 9){
+  		$scope.convert();
+  	} 
+  }
+
+  $scope.wipeBaseInput = function(eleId){
+  	if (eleId == 'base-input'){
+	  	$scope.base.value = "";
+	  	$scope.base.lastValue = "";
+      $scope.result.value = "";
+	} else if (eleId == 'base-input-2'){
+	  	$scope.base.value2 = "";
+	  	$scope.base.lastValue2 = "";
+	}
+  	$timeout(function(){
+  		document.getElementById(eleId).focus();
+  	}, 50)
+  	
+  }
+
+  $rootScope.$on("online", function(event, isOnline){
+    if (isOnline == true){
+      $scope.initAdMob();
+    } else {
+      $scope.removeAdMob();
+    }
+  })
 })
--- js/services.js
+++ js/services.js
@@ -1,4 +1,4 @@
-angular.module('converter.services', [])
+angular.module('converter.services', ['LocalStorageModule'])
 
 /**
  * A simple example service that returns some data.
@@ -8,12 +8,16 @@ angular.module('converter.services', [])
 
   // Some fake testing data
   var units = [
-    { id: 0, name: 'bananas', type: "Universal" },
-    { id: 1, name: 'centimeters', type: "Metric" },
-    { id: 1, name: 'millimeters', type: "Metric" },
-    { id: 2, name: 'inches', type: "Imperial" },
-    { id: 3, name: 'feet', type: "Imperial" },
-    { id: 4, name: 'meters', type: "Metric" }
+    { name: 'bananas', type: "Universal units", typeId: "0", symbol : 'bn' },
+    { name: 'inches', type: "Imperial units", typeId: "1", symbol : 'in'},
+    { name: 'feet', type: "Imperial units", typeId: 1, symbol : 'ft' },
+    { name: 'feet and inches', type: "Imperial units", typeId: 1, symbol : 'ft in' },
+    { name: 'yards', type: "Imperial units", typeId: 1, symbol : 'yd' },
+    { name: 'miles', type: "Imperial units", typeId: 1, symbol : 'mi' },
+    { name: 'millimeters', type: "Metric units", typeId: 2, symbol : 'mm' },
+    { name: 'centimeters', type: "Metric units", typeId: 2, symbol : 'cm' },
+    { name: 'meters', type: "Metric units", typeId: 2, symbol : 'm' },
+    { name: 'kilometers', type: "Metric units", typeId: 2, symbol : 'km' }
   ];
   // var qties = new Qty('m');
 
@@ -38,4 +42,261 @@ angular.module('converter.services', [])
       return deferred.promise;
     }
   }
-});
+})
+
+
+
+.factory('ConversionLocalStorageService', function($q, localStorageService){
+  var factory = [];
+
+  factory.addToConversionHistory = function(scope){
+    var deferred = $q.defer();
+
+    var conversion = {
+      baseValue : scope.base.value,
+      baseValue2 : scope.base.value2,
+      baseUnit : scope.base.unit.name,
+      resultValue : scope.result.value,
+      resultValue2 : scope.result.value2,
+      resultUnit : scope.result.unit.name,
+      kindName : scope.kind
+    };
+
+    var conversionHistory = localStorageService.get('conversionHistory');
+
+    /* Do not add if last conversion is the same as the new one */
+    if (JSON.stringify(conversionHistory[conversion.kindName][0]) != JSON.stringify(conversion))
+      conversionHistory[conversion.kindName].unshift(conversion);
+
+    if (conversionHistory[conversion.kindName].length > 10)
+      conversionHistory[conversion.kindName].pop();
+
+    localStorageService.set('conversionHistory', conversionHistory);
+
+    scope.conversionHistory = conversionHistory[conversion.kindName];
+
+    deferred.resolve(conversionHistory[conversion.kindName]);
+
+    return deferred.promise;
+
+  };
+
+  factory.loadHistory = function(scope){
+    /* Check if history is already set conversions in localStorage */
+    if (!localStorageService.get('conversionHistory')){
+      localStorageService.set('conversionHistory', {});
+    }
+
+
+    /* Check if history exists for this kind of unit conversion */
+    var conversionHistory = localStorageService.get('conversionHistory');
+    if (!conversionHistory.hasOwnProperty(scope.kind))
+      conversionHistory[scope.kind] = [];
+
+    localStorageService.set('conversionHistory', conversionHistory);
+
+    scope.conversionHistory = conversionHistory[scope.kind];
+
+  };
+
+  factory.saveLastUsedBaseUnit = function(scope){
+    localStorageService.set('lastUsedBaseUnit', scope.base.unit);
+  };
+
+  factory.saveLastUsedResultUnit = function(scope){
+    localStorageService.set('lastUsedResultUnit', scope.result.unit);
+  }
+
+  factory.loadLastUsedUnits = function(scope){
+    var deferred = $q.defer();
+
+    if (!localStorageService.get('lastUsedBaseUnit') || !localStorageService.get('lastUsedResultUnit')){
+      deferred.reject(false);
+    } else {
+      scope.base.unit = localStorageService.get('lastUsedBaseUnit');
+      scope.result.unit = localStorageService.get('lastUsedResultUnit');
+      deferred.resolve(true);
+    }
+
+    return deferred.promise;
+  }
+
+  factory.wipe = function(scope){
+    var conversionHistory = localStorageService.get('conversionHistory');
+    conversionHistory[scope.kind] = [];
+    localStorageService.set('conversionHistory', conversionHistory);
+    scope.conversionHistory = [];
+
+  }
+
+  return factory;
+
+})
+
+
+.factory('ConversionModel', function($q){
+  var factory = [];
+
+  factory.resultValueAndUnitString = function(conversion){
+    return conversion.resultValue + " " + conversion.resultUnit;
+  }
+
+  factory.toString = function(conversion){
+    var leftSide = "";
+    var rightSide = "";
+    if (conversion.baseUnit == "feet and inches"){
+      leftSide = parseFloat(0+conversion.baseValue) + "ft " + parseFloat(0+conversion.baseValue2) + 'in';
+    }else {
+      leftSide =  parseFloat(0+conversion.baseValue) + " " + conversion.baseUnit;
+    }
+
+    if (conversion.resultUnit == "feet and inches"){
+      rightSide = parseFloat(0+conversion.resultValue) + "ft " + parseFloat(0+conversion.resultValue2) + 'in';
+    }else {
+      rightSide =  parseFloat(0+conversion.resultValue) + " " + conversion.resultUnit;
+    }
+
+    return leftSide + " = " + rightSide;
+  }
+
+  factory.rightSideToString = function(conversion){
+    if (conversion.resultUnit == "feet and inches"){
+      rightSide = parseFloat(0+conversion.resultValue) + "ft " + parseFloat(0+conversion.resultValue2) + 'in';
+    }else {
+      rightSide =  parseFloat(0+conversion.resultValue) + " " + conversion.resultUnit;
+    }
+    return rightSide;
+  }
+
+
+  factory.getBaseQty = function(scope){
+    if (scope.base.unit.name == "feet and inches") {
+      var baseQtyFeet = new Qty(scope.base.value + "feet");
+      if (scope.base.value2 == "") scope.base.value2 = 0;
+      var baseQtyInches = new Qty(scope.base.value2 + "inches");
+      console.log(scope.base.value2)
+
+      var totalQtyInches = new Qty(baseQtyFeet.to("inches").scalar + baseQtyInches.scalar + "inches");
+
+      return totalQtyInches;
+
+    } else {
+      try {
+        return Qty(scope.base.value + " " + scope.base.unit.name);
+      } catch (exception){
+        return exception;
+      }
+    }
+  }
+
+  factory.setResult = function(scope){
+    if (scope.result.unit.name == "feet and inches") {
+      var totalInches = new Qty(scope.result.value + "inches");
+      var totalFeet = totalInches.to("feet");
+      var qtyFeet = new Qty(Math.floor(totalFeet.scalar) + "feet");
+      var qtyInches = new Qty(totalFeet.scalar - qtyFeet.scalar + "feet").to("inches").toPrec(0.01);
+
+      scope.result.value = parseFloat(0+qtyFeet.scalar);
+      scope.result.value2 = qtyInches.scalar;
+    }
+
+    if (scope.result.value < 0.01 && scope.result.value > 0 || scope.result.value > 9999999999)
+      scope.result.value = scope.result.value.toExponential(3);
+    else
+      scope.result.value = parseFloat(scope.result.value.toFixed(3).replace(".000", ""));
+
+    if (scope.result.value2 != "" ) {
+      if (scope.result.value2 < 0.01 && scope.result.value2 > 0 || scope.result.value2 > 9999999999)
+        scope.result.value2 = scope.result.value2.toExponential(3);
+      else
+        scope.result.value2 = parseFloat(scope.result.value2.toFixed(3).replace(".000", ""));
+    }
+
+
+  }
+
+  return factory;
+
+})
+
+
+/**
+* @class angular_module.steroidsBridge.ConnectionManager
+* @classdesc Service pour gérer les événements en-ligne, hors-ligne
+*/
+.factory("ConnectionManager", function(localStorageService, $rootScope, $timeout){
+
+  localStorageService.set("online", "");
+  var connectionManager = [];
+
+  /* Flags créés pour éviter les appels multiples lors des changements d'états */
+  var offlineFnCallBuffer = false;
+  var onlineFnCallBuffer = false;
+
+
+ /**
+  * @name onOffline
+  * @function
+  * @memberOf angular_module.steroidsBridge.ConnectionManager
+  * @description Méthode appelée lorsque l'application redevient en ligne
+  */
+  connectionManager.onOffline = function() {
+    if (localStorageService.get("online") != false && !offlineFnCallBuffer) { 
+      offlineFnCallBuffer = true;
+      localStorageService.set("online", false);
+
+      /* On notifie les observateurs */
+      $rootScope.$broadcast('online', false);
+
+      $timeout(function(){
+        offlineFnCallBuffer = false;
+      }, 500)
+
+    }
+  }
+
+
+ /**
+  * @name onOnline
+  * @function
+  * @memberOf angular_module.steroidsBridge.ConnectionManager
+  * @description Méthode appelée lorsque l'application devient hors-ligne
+  */
+  connectionManager.onOnline = function(){
+    if (localStorageService.get("online") != true && !onlineFnCallBuffer) {
+      onlineFnCallBuffer = true;
+
+      /* On notifie les observateurs */
+      $rootScope.$broadcast('online', true); 
+
+      localStorageService.set("online", true);
+      $timeout(function(){
+        onlineFnCallBuffer = false;
+      }, 500)
+
+    }
+  }
+
+
+  connectionManager.isOnline = function(){
+    return (localStorageService.get("online") == "true") ? true : false;
+  }
+
+
+  /* Gestionnaires d'événements -> ces événements sont lancés par Cordova */
+  connectionManager.addEventListeners = function(){
+    document.addEventListener("offline", connectionManager.onOffline, false);
+    document.addEventListener("online", connectionManager.onOnline, false);
+  }
+  connectionManager.removeEventListeners = function(){
+    document.removeEventListener("offline", connectionManager.onOffline, false);
+    document.removeEventListener("online", connectionManager.onOnline, false); 
+  }
+
+
+  connectionManager.addEventListeners();
+
+  return connectionManager;
+
+
+})
